d5b30cb76cafd9648844e7bb4e9ad2b95e3cc3a1
maolele <m01.sgwk@gmail.com>
2020-08-01 22:59:11 +0900

    Answer Q4-Q7

--- task.rb
+++ task.rb
@@ -32,7 +32,7 @@ def q4
   sports = ["サッカー", "フットサル", nil, "野球", "バスケ", nil, "バレー"]
 
   # 以下に回答を記載
-
+  sports.delete(nil)
   # 以下は変更しないで下さい
   p sports
 end
@@ -42,21 +42,26 @@ def q5
   array2 = [1, 5, 8, 10]
 
   # 以下に回答を記載
-
+  p array1.empty?
+  p array2.empty?
 end
 
 def q6
   numbers1 = [1, 2, 3, 4, 5]
+  numbers2 = []
 
   # 以下に回答を記載
-
+  numbers1.each do |number|
+    numbers2.push(number * 10)
+  end
+  p numbers2
 end
 
 def q7
   array = ["1", "2", "3", "4", "5"]
 
   # 以下に回答を記載
-
+  array.map!(&:to_i)
   # 以下は変更しないで下さい
   p array
 end
