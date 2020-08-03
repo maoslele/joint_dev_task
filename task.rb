76e3267c3dbf4580bb35f580ef25e5e02a0140e8
maoslele <m01.sgwk@gmail.com>
2020-08-03 18:48:54 +0900

    Modifed answer Q8, Q11

--- task.rb
+++ task.rb
@@ -67,7 +67,7 @@ def q8
   programming_languages = %w(ruby php python javascript)
 
   # 以下に回答を記載
-  programming_languages = programming_languages.map(&:capitalize)
+  programming_languages.map!(&:capitalize)
   upper_case_programming_languages = programming_languages.map(&:upcase)
   # 以下は変更しないで下さい
   p programming_languages
@@ -97,9 +97,7 @@ def q11
 
   # 以下に回答を記載
   puts "ユーザーの趣味一覧"
-  flattes_sports = sports.flatten
-  sports_new = flattes_sports.uniq
-  sports_new.each.with_index(1) do |sport, index|
+  sports.flatten.uniq.each.with_index(1) do |sport, index|
     puts "No#{index} #{sport}"
   end
 end
