8b4253565f7dd9af3d6c3277e74f11874d2856f8
maolele <m01.sgwk@gmail.com>
2020-08-03 13:04:59 +0900

    Modified answer Q8-11

--- task.rb
+++ task.rb
@@ -67,6 +67,7 @@ def q8
   programming_languages = %w(ruby php python javascript)
 
   # 以下に回答を記載
+  programming_languages = programming_languages.map(&:capitalize)
   upper_case_programming_languages = programming_languages.map(&:upcase)
   # 以下は変更しないで下さい
   p programming_languages
@@ -77,7 +78,7 @@ def q9
   names = ["田中", "佐藤", "佐々木", "高橋"]
 
   # 以下に回答を記載
-  names.each_with_index do |name, index|
+  names.each.with_index(1) do |name, index|
     puts "会員No.#{index} #{name}さん"
   end
 end
@@ -87,11 +88,7 @@ def q10
 
   # 以下に回答を記載
   foods.each do |food|
-    if food.include?("うに")
-      puts "好物です"
-    else
-      puts "まぁまぁ好きです"
-    end
+    puts food.include?("うに") ? "好物です" : "まぁまぁ好きです"
   end
 end
 
@@ -100,8 +97,9 @@ def q11
 
   # 以下に回答を記載
   puts "ユーザーの趣味一覧"
-  sports.each_with_index do |sport, index|
-    puts "NO#{index} #{sport}"
+  flattes_sports = sports.flatten
+  flattes_sports.each.with_index(1) do |sport, index|
+    puts "No#{index} #{sport}"
   end
 end
 
